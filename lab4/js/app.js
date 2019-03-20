4d96c622fab795290f597f1ac87ae1d571db1988
Lorien <lorientheunissen@.me.com>
2019-03-20 22:12:45 +0100

    API / constructor

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,7 +1,8 @@
 class Weather
 {
-    constructor(options){
-        
+    constructor(API_KEY){
+        this.API_KEY = API_KEY;
+        //console.log(API_KEY);
     }
 
     getCurrentLocation(){
@@ -17,14 +18,8 @@ class Weather
     }
 
     init(){
-        
     }
 
 }
 
-
-const options = {
-    'apikey': '',
-    'el': '#app'
-}
-const App = new Weather(options);
+let app = new Weather('feb187c5167915f71c0d58ae86c01760');
