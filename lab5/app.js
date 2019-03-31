ebc1383189399b6eda94a275f909d86bf0ac39bf
Lorien <r0622279@student.thomasmore.be>
2019-03-31 22:37:45 +0200

    Node / basic app setup

--- lab5/app.js
+++ lab5/app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
