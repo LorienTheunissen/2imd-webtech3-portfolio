84c0532cfd751fa56dc60ea4444afbfbcfbdd624
Lorien <r0622279@student.thomasmore.be>
2019-04-02 00:05:15 +0200

    Okay! Let's start over & try again 👊🏻

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -9,25 +9,20 @@ sleet
 snow
 wind
 fog
-
-ABBREVIATIONS
-AKW = API key Weather
-AKG = API key Giphy
 */
 
 
 class Weather
 {
-    constructor(AKW, AKG){
-        this.AKW = AKW;
-        this.AKG = AKG;
-        //console.log(AKW, AKG);
+    constructor(API_KEY){
+        this.API_KEY = API_KEY;
+        //console.log(API_KEY);
         this.init();
     }
 
     getCurrentLocation(){
-        //console.log("getting current position");
-        navigator.geolocation.getCurrentPosition(position =>{
+        console.log("getting current position");
+        navigator.geolocation.getCurrentPosition(position => {
             let lat = position.coords.latitude;
             let lng = position.coords.longitude;
             this.getWeather(lat, lng);
@@ -48,58 +43,8 @@ class Weather
             temp.innerHTML = json.currently.summary;
             document.querySelector(".container").appendChild(temp);
             console.log(json.currently.summary);
-
-            let gif = json.currently.icon;
-            switch(gif){
-                case "clear-day":
-                    this.getGiphy("clear day");
-                    break;
-                case "clear-night":
-                    this.getGiphy("");
-                    break;
-                case "partly-cloudy-day":
-                    this.getGiphy("");
-                    break;
-                case "partly-cloudy-night":
-                    this.getGiphy("");
-                    break;
-                case "cloudy":
-                    this.getGiphy("");
-                    break;
-                case "rain":
-                    this.getGiphy("");
-                    break;
-                case "sleet":
-                    this.getGiphy("");
-                    break;
-                case "snow":
-                    this.getGiphy("");
-                    break;
-                case "wind":
-                    this.getGiphy("");
-                    break;
-                case "fog":
-                    this.getGiphy("");
-                    break;
-                default:
-                    this.getGiphy(gif);
-                    break;
-            }
         })
     }
-
-    getGiphy(gif){
-        // https://developers.giphy.com/docs/#sticker-endpoint
-        let url = `//api.giphy.com/v1/stickers/search?api_key=${this.AKG}&q=${gif}`;
-        fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(json => {
-            // GET GIPHY
-        })
-    }
-
 
     init(){
         //console.log('initialize');
@@ -109,4 +54,4 @@ class Weather
 
 }
 
-let app = new Weather('feb187c5167915f71c0d58ae86c01760', 'GNAEtDyVKZCH4mCW2LxrD6omQbpmpdHO');
+let app = new Weather('feb187c5167915f71c0d58ae86c01760');
