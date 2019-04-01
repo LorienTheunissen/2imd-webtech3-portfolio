89b7f225907f385da594953a7fdd0248aeeba3d4
Lorien <r0622279@student.thomasmore.be>
2019-04-01 07:57:18 +0200

    API / switch case

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -9,15 +9,19 @@ sleet
 snow
 wind
 fog
+
+ABBREVIATIONS
+AKW = API key Weather
+AKG = API key Giphy
 */
 
 
 class Weather
 {
-    constructor(API_KEY_W, API_KEY_G){
-        this.API_KEY_W = API_KEY_W;
-        this.API_KEY_G = API_KEY_G;
-        //console.log(API_KEY_W, API_KEY_G);
+    constructor(AKW, AKG){
+        this.AKW = AKW;
+        this.AKG = AKG;
+        //console.log(AKW, AKG);
         this.init();
     }
 
@@ -34,7 +38,7 @@ class Weather
     }    
 
     getWeather(lat, lng){
-        let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY_W}/${lat},${lng}?units=si`;
+        let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.AKW}/${lat},${lng}?units=si`;
         fetch(url)
         .then(response => {
             return response.json();
@@ -42,13 +46,51 @@ class Weather
         .then(json => {
             let temp = document.createElement("h1");
             temp.innerHTML = json.currently.summary;
-            document.querySelector("#container").appendChild(temp);
+            document.querySelector(".container").appendChild(temp);
             console.log(json.currently.summary);
+
+            let gif = json.currently.icon;
+            switch(gif){
+                case "clear-day":
+                    this.getGiphy("clear day");
+                    break;
+                case "clear-night":
+                    this.getGiphy("");
+                    break;
+                case "partly-cloudy-day":
+                    this.getGiphy("");
+                    break;
+                case "partly-cloudy-night":
+                    this.getGiphy("");
+                    break;
+                case "cloudy":
+                    this.getGiphy("");
+                    break;
+                case "rain":
+                    this.getGiphy("");
+                    break;
+                case "sleet":
+                    this.getGiphy("");
+                    break;
+                case "snow":
+                    this.getGiphy("");
+                    break;
+                case "wind":
+                    this.getGiphy("");
+                    break;
+                case "fog":
+                    this.getGiphy("");
+                    break;
+                default:
+                    this.getGiphy(gif);
+                    break;
+            }
         })
     }
 
     getGiphy(gif){
-        let url = `//api.giphy.com/v1/gifs/search?api_key=${this.API_KEY_G}&q=${gif}`;
+        // https://developers.giphy.com/docs/#sticker-endpoint
+        let url = `//api.giphy.com/v1/stickers/search?api_key=${this.AKG}&q=${gif}`;
         fetch(url)
         .then(response => {
             return response.json();
