701cad1d368752c1cf2ef1c803632f3bf55acbbe
Lorien <r0622279@student.thomasmore.be>
2019-04-02 11:43:59 +0200

    API / Added API_KEY_GIPHY

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -1,23 +1,17 @@
-/* GIPHY ICONS VALUES via https://www.dr-lex.be/software/darksky-icons.html
-clear-day
-clear-night
-partly-cloudy-day
-partly-cloudy-night
-cloudy
-rain
-sleet
-snow
-wind
-fog
-*/
-
-
 class Weather
 {
-    constructor(API_KEY){
-        this.API_KEY = API_KEY;
-        //console.log(API_KEY);
+    constructor(API_KEY_WEATHER, API_KEY_GIPHY){
+        this.API_KEY_WEATHER = API_KEY_WEATHER;
+        this.API_KEY_GIPHY = API_KEY_GIPHY;
+        console.log(API_KEY_WEATHER, API_KEY_GIPHY);
         this.init();
+    }
+
+    init(){
+        //console.log('initialize');
+        console.log(navigator);
+        this.getCurrentLocation();
+        this.getSticker();
     }
 
     getCurrentLocation(){
@@ -33,25 +27,24 @@ class Weather
     }    
 
     getWeather(lat, lng){
-        let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.AKW}/${lat},${lng}?units=si`;
+        let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY_WEATHER}/${lat},${lng}?units=si`;
         fetch(url)
         .then(response => {
             return response.json();
         })
         .then(json => {
             let temp = document.createElement("h1");
-            temp.innerHTML = json.currently.summary;
-            document.querySelector(".container").appendChild(temp);
+            temp.innerHTML = `This is what the weather looks like today: ${json.currently.summary}`;
+            document.querySelector("#app").appendChild(temp);
             console.log(json.currently.summary);
         })
     }
 
-    init(){
-        //console.log('initialize');
-        console.log(navigator);
-        this.getCurrentLocation();
+    getSticker(){
+        console.log("🚀");
     }
-
 }
 
-let app = new Weather('feb187c5167915f71c0d58ae86c01760');
+let app = new Weather('feb187c5167915f71c0d58ae86c01760', 'GNAEtDyVKZCH4mCW2LxrD6omQbpmpdHO');
+
+// https://developers.giphy.com/docs/
